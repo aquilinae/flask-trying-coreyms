85d68e14b9b51418712053aee929db292b16787d
ornichola <1502708+ornichola@users.noreply.github.com>
2021-03-25 14:43:08 +0300

    Using templates

--- app.py
+++ app.py
@@ -1,12 +1,37 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+    url_for,
+)
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+
+POSTS = [
+    {
+        'author': 'ornichola',
+        'title': 'First post',
+        'content': 'Lorem ipsum',
+        'date_posted': 'March 24, 2021',
+    },
+    {
+        'author': 'not_ornichola',
+        'title': 'Second post',
+        'content': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus id ornare lorem. Duis at imperdiet tortor, vel ullamcorper velit. Vivamus euismod ipsum et nibh consequat ultricies. Donec et tortor dui. Sed iaculis in tortor vitae ultrices. Aenean quis orci a tellus bibendum egestas ac nec neque. Cras viverra nunc in turpis lobortis porttitor. Suspendisse potenti. Praesent fringilla, urna id ullamcorper egestas, felis purus vestibulum sapien, sollicitudin facilisis dolor dui ac ligula. Donec sit amet risus id nisi viverra fringilla nec ac ligula.',
+        'date_posted': 'March 25, 2021',
+    }
+]
 
 
 @app.route('/')
-def hello():
-    return '<h1>Hello, world!</h1>'
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=POSTS)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
